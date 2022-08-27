3a5549d34fc6a1ed61c8b6a04792ccfed1ae6c73
chassis360 <chassis247@gmail.com>
2022-08-27 20:47:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# JavaScript Tutorial
+
+## Description
+This is a source code for those who are interested in learning JavaScript.
---
+++ js/external_javascript.js
@@ -0,0 +1,6 @@
+//Sigle Element Selector
+// console.log(document.getElementById('my-form')) ;
+// console.log(document.querySelector('h1')) 
+
+//Multiple Element Selector
+document.querySelectorAll('')
